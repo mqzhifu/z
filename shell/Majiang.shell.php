16eecb3c3ec47a85931808cc7c003b7cbcff1772
mqzhifu <mqzhifu@sina.com>
2018-03-26 23:12:16 +0800

    aa
    aaa

--- shell/Majiang.shell.php
+++ shell/Majiang.shell.php
@@ -1,6 +1,6 @@
 <?php
-include '../lib/redisPHP.lib.php' ;
-class Chat{
+//include '../lib/redisPHP.lib.php' ;
+class Majiang{
 
 	private $redis ;
 
@@ -10,8 +10,8 @@ class Chat{
 	const STATUS_AUTO = 4 ;
 
     function __construct($c){
-        $this->commands = $c;
-		$this->redis = new RedisPHPLib() ;
+//        $this->commands = $c;
+//		$this->redis = new RedisPHPLib() ;
     }
 
     public function run($attr){
@@ -20,44 +20,56 @@ class Chat{
 
 		//创建websocket服务器对象，监听0.0.0.0:9502端口
 		$ws = new swoole_websocket_server($ip, $port);
-        $ws->set(array(
-            'worker_num'=>4
-        ));
-
-		//监听WebSocket连接打开事件.
-		$ws->on('open', function ($ws, $request) {
-			echo "server: handshake success with fd{$request->fd}\n";
+//        $ws->set(array(
+//            'worker_num'=>4
+//        ));
 
 
-		});
+        $ws->on('open', function (swoole_websocket_server $ws, $request) {
+            echo "server: handshake success with fd{$request->fd}\n";
+        });
 
-		//监听WebSocket消息事件
-		$ws->on('message', function ($ws, $frame) {
-			echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
-			$_data = json_decode($frame->data, true) ;
+        $ws->on('message', function (swoole_websocket_server $ws, $frame) {
+            echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
+            $ws->push($frame->fd, "this is server");
+        });
 
-			switch ($_data['req_action']){
-				case 'connect' :
-                    // 客户端连接
-                    $this->clientConnect($ws, $frame, $_data) ;
-                    break;
-                case 'heartbeat':
-                    // 心跳处理
-                    break;
-                default:
-                    // action_xxx 处理
-                    $this->doAction($ws, $frame, $_data) ;
-                    null ;
-			}
 
-		});
 
-		//监听WebSocket连接关闭事件
-		$ws->on('close', function ($ws, $fd) {
-			echo "client-{$fd} is closed\n";
-		});
-
-		$ws->start();
+//		//监听WebSocket连接打开事件.
+//		$ws->on('open', function ($ws, $request) {
+//			echo "server: handshake success with fd{$request->fd}\n";
+//
+//
+//		});
+//
+//		//监听WebSocket消息事件
+//		$ws->on('message', function ($ws, $frame) {
+//			echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
+//			$_data = json_decode($frame->data, true) ;
+//
+//			switch ($_data['req_action']){
+//				case 'connect' :
+//                    // 客户端连接
+//                    $this->clientConnect($ws, $frame, $_data) ;
+//                    break;
+//                case 'heartbeat':
+//                    // 心跳处理
+//                    break;
+//                default:
+//                    // action_xxx 处理
+//                    $this->doAction($ws, $frame, $_data) ;
+//                    null ;
+//			}
+//
+//		});
+//
+//		//监听WebSocket连接关闭事件
+//		$ws->on('close', function ($ws, $fd) {
+//			echo "client-{$fd} is closed\n";
+//		});
+//
+//		$ws->start();
 
 	}
 
